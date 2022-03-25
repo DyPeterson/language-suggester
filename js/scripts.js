779a32e94310a767b7c1b7eaa1506f5817fa3078
Dylan Peterson <dylan.peterson17@gmail.com>
2022-03-25 09:57:44 -0700

    add javascript functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,26 @@
 $(document).ready(function() {
   $("form").submit(function(event){
     event.preventDefault();
-    
+    const result1 = parseInt($("#question1").val());
+    const result2 = parseInt($("#question2").val());
+    const result3 = parseInt($("#question3").val());
+    const result4 = parseInt($("#question4").val());
+    const result5 = parseInt($("#question5").val());
+    const result = result1 + result2 + result3 + result4 + result5;
+
+    if (result <= 3) {
+      $("#ruby").show();
+    }
+    else if (result > 3 && result < 8) {
+      $("#javascript").show();
+      $("#ruby").hide();
+    }
+    else if (result >= 8) {
+      $("#python").show();
+      $("#javascript").hide();
+    }
+    else {
+      prompt("Please fill out all the answers!")
+    }
   });
 });
